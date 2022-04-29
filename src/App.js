b125915079dfb41753bffd9c25f2eaab8f95eed9
shaimun-sharin <shaimunsharin@gmail.com>
2022-04-29 23:25:49 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const [foods, setFoods] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/food")
+      .then((res) => res.json())
+      .then((data) => setFoods(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Data load :{foods.length}</h1>
+      <ul>
+        {foods.map((food) => (
+          <li key={food._id}>
+            <h3>{food.name}</h3> <br />
+            <img width={60} src={food.picture} alt="" /> <br />
+            <p>{food.description}</p> <br />
+            <h5>{food.supplierName}</h5>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
