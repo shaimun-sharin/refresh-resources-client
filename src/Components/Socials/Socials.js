1276590b2dc1b1dae672b77d57ab30e0f381ecd5
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-05 14:38:58 +0600

    google login enabled

--- src/Components/Socials/Socials.js
+++ src/Components/Socials/Socials.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Loading/Loading";
+import { useNavigate } from "react-router-dom";
 
 const Socials = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  if (user) {
+    navigate("/home");
+  }
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  let errorElement;
+  if (error) {
+    errorElement = <h6 className="text-danger">Error: {error?.message}</h6>;
+  }
+
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -10,8 +27,12 @@ const Socials = () => {
         <p className="mt-2 px-4 text-white">OR</p>
         <div style={{ height: "1px" }} className="bg-secondary w-50"></div>
       </div>
+      {errorElement}
       <div>
-        <button className="btn btn-outline-success w-75 text-center d-block mx-auto">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline-success w-75 text-center d-block mx-auto"
+        >
           <FontAwesomeIcon className="px-2" icon={faGoogle} /> Continue with
           Google
         </button>
