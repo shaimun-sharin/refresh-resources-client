f1b7303412d701b4d3f00ea0a3c56f31ae6e6470
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-05 22:11:23 +0600

    delete enabled

--- src/Components/ManageInventory/Manageinventory.js
+++ src/Components/ManageInventory/Manageinventory.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 import useFoods from "../hooks/useFoods";
 
 const Manageinventory = () => {
-  const [foods] = useFoods([]);
+  const [foods, setFoods] = useFoods([]);
+  const handleDelete = (id) => {
+    const proceed = window.confirm("are you sure?");
+    if (proceed) {
+      const url = `http://localhost:5000/food/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaining = foods.filter((food) => food._id !== id);
+          setFoods(remaining);
+        });
+    }
+  };
   return (
     <div>
       <h1 className="text-center text-success p-4">Inventory</h1>
@@ -24,13 +41,23 @@ const Manageinventory = () => {
                 <td>{food.price}</td>
                 <td>{food.quantity}</td>
                 <td>
-                  <button className="btn btn-danger">Delete</button>
+                  <button
+                    onClick={() => handleDelete(food._id)}
+                    className="btn btn-danger"
+                  >
+                    Delete X
+                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </Table>
       </div>
+      <button className="btn btn-success d-block w-25 mt-4 m-auto">
+        <Link className="text-decoration-none text-white" to="/addFood">
+          Add New Item
+        </Link>
+      </button>
     </div>
   );
 };
